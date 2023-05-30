400e0a60fe3845794660ccfc4cfb306f0b019dba
Ashenafi Goitom <ashgoitom@gmail.com>
2023-05-30 12:59:46 -0700

    Add python script that initialize a square with a size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contians a square definition
+"""
+
+
+class Square:
+    """A square class
+    """
+
+    def __init__(self, size):
+        """Initialize the class with a data
+
+        Args:
+            size: size of the square
+
+        Attributes:
+            size: size of the square
+
+        """
+        self.__size = size
