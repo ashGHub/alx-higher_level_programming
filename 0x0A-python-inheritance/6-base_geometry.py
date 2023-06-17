2a609c8afa210403de5afe6c097349d25831732e
Ashenafi Goitom <ashgoitom@gmail.com>
2023-06-17 14:10:22 -0700

    Add python inheritance task 6 solution

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module for base geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Base Geometry class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
