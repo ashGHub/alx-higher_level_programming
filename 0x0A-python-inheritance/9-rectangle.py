4b7623b72c0b650544eee5684c9a8af0a6eb6bbe
Ashenafi Goitom <ashgoitom@gmail.com>
2023-07-08 07:29:00 -0700

    Add task 9 solution for python inheritance

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module for rectangle base geometry
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle Base Geometry class
+    """
+
+    def __init__(self, width, height):
+        """
+        initializes base geometry class
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def area(self):
+        return self.__width * self.__height
