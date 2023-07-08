ae12d27509d1c305bc63ccf8f98540216f0ac986
Ashenafi Goitom <ashgoitom@gmail.com>
2023-07-08 07:42:55 -0700

    Add task 11 solution for python inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module for square base geometry
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square Base Geometry class
+    """
+
+    def __init__(self, size):
+        """
+        initializes square instance
+
+        Args:
+            size (int): sizeo of square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        calculates area of square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """
+        returns string representation for square class
+        """
+        return f"[Square] {self.__size}/{self.__size}"
