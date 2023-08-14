0ece43f7c712c98322a03070267898e7da9b907d
Ashenafi Goitom <ashgoitom@gmail.com>
2023-08-14 13:45:51 -0700

    Add task 9 solution for javascript warm up

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if ((a == 0 || a) && (b == 0 || b))
+{
+	console.log(add(a, b));
+}
+else
+{
+	console.log("NaN");
+}
+
+function add(a, b)
+{
+	return a + b;
+}
