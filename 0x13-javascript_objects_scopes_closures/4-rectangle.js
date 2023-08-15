a25508b66d74d659bf7ed721b1d9fe612e6ec7fa
Ashenafi Goitom <ashgoitom@gmail.com>
2023-08-16 00:09:33 +0300

    Fix code style for javascript objects scopes closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-  cr = 'X';
-
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
+    this.cr = 'X';
   }
 
   print () {
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Sq = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Sq {
   charPrint (c) {
     if (c) {
       super.cr = c;
