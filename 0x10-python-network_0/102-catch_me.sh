6ede3b7e696da95c4ffa7b1457cff28af0f8125e
Ashenafi Goitom <ashgoitom@gmail.com>
2023-09-06 23:42:20 +0300

    Fix a bug on python network 0 task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to
-curl -sX PUT -d "message=You got me!" 0.0.0.0:5000/catch_me
+# I only figured the parameters from the testcases after first deadline
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
