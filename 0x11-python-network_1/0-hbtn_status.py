75073b0a9c19d0a46958c12deb52ef132193d9c4
Ashenafi Goitom <ashgoitom@gmail.com>
2023-09-06 22:26:07 +0300

    Fix task 0 solution

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """Script to fetch https://intranet.hbtn.io/status"""
 
+
+def print_response(res):
+    """Prints the response of a URL request"""
+    html = res.read()
+    print("Body response:")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
+
+
 if __name__ == "__main__":
     from urllib import request
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(html)}")
-        print(f"\t- content: {html}")
-        print(f"\t- utf8 content: {html.decode('utf-8')}")
+    try:
+        with request.urlopen('http://0.0.0.0:5050/status') as response:
+            print_response(response)
+    except Exception as e:
+        with request.urlopen('https://intranet.hbtn.io/status') as response:
+            print_response(response)
