8c054c9e678cfd4bdb87899891f7f982475b9057
Ashenafi Goitom <ashgoitom@gmail.com>
2023-09-06 22:37:44 +0300

    Add task 7 solution

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Makes a request and displays response body"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print(f"Error code: {res.status_code}")
+    else:
+        print(res.text)
