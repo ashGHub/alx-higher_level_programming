2cbe9196ddd50de2d359a1321220ae5f6d50ec30
Ashenafi Goitom <ashgoitom@gmail.com>
2023-09-09 18:56:38 +0300

    Add task 100 solution python ORM

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Creates the State “California” with the City “San Francisco”
+from the database hbtn_0e_100_usa
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
+    user = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{pw}@localhost/{db}',
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='California')
+    new_state.cities = [City(name='San Francisco')]
+    session.add(new_state)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a City class to work with MySQLAlchemy ORM."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """Represents a city in a MySQL database."""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
