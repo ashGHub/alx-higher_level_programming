c886099a87729184f676118247a859a1f196a220
Ashenafi Goitom <ashgoitom@gmail.com>
2023-09-09 14:51:53 +0300

    Add task 2 solution python ORM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Filter a state on states list from the database hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db_conn = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+    cursor = db_conn.cursor()
+    cursor.execute("SELECT * \
+                   FROM states \
+                   WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db_conn.close()
