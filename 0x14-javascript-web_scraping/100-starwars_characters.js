23c8b792895f226004a53010845862a0aa2fc02d
Ashenafi Goitom <ashgoitom@gmail.com>
2023-09-27 16:31:32 +0300

    Add task 100 solution javascript web scraping

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    return console.log(err);
+  }
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    request(character, function (err, response, body) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
