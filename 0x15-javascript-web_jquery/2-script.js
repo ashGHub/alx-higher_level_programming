5a048d3448b767c818518b8c0f5dedd5ed6c903f
Ashenafi Goitom <ashgoitom@gmail.com>
2023-10-04 22:55:48 +0300

    Add task 2 solution javascript jquery

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+// updates the text color of the header element to red
+// when the user clicks on the tag DIV#red_header
+$(function ()
+{
+  $('DIV#red_header').click(function () {
+    $('header').css('color', 'red');
+  });
+});
