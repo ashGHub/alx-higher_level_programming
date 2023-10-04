ddbbd7a26b1a7dba89cd8f4efcae346da55dc8cc
Ashenafi Goitom <ashgoitom@gmail.com>
2023-10-04 23:07:52 +0300

    Add task 5 solution javascript jquery

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+// adds a li element to a list
+// when the user clicks on the tag DIV#add_item
+$(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+});
